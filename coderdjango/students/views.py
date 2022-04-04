da43988655418fd8b227ed858d62314cedb9c55f
franco-1985 <franco1985@gmail.com>
2022-04-03 21:28:58 -0300

    b

--- coderdjango/students/views.py
+++ coderdjango/students/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from coderdjango.students.models import Familiar
+from coderdjango.students.models import Familiar, Curso
 
 class StudentView(TemplateView):
     template_name = 'students/index.html'
@@ -9,10 +9,15 @@ class StudentView(TemplateView):
     # def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
     #     return super().get_context_data(**kwargs)
 
+
     def get(self, request, status=None):
-        print(request.GET.get('edad'))
+
+        cursos = Curso.objects.all()
+
+        #print(request.GET.get('edad'))
         context = {
-            'edad': request.GET.get('edad'),
-            'list_test': [Familiar(),Familiar(),Familiar(),Familiar()]
+            #'edad': request.GET.get('edad'),
+            # 'list_test': [curso,Familiar(),Familiar(),Familiar()]
+            'list_test': cursos
         }
         return render(request, self.template_name, context)
